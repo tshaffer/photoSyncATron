2dc431871aecc2eb57c16415fcf9656d014d1015
Ted Shaffer <shaffer.family@gmail.com>
2017-01-01 07:35:36 -0800

    Screen layout / content improvements.

--- src/components/landing.js
+++ src/components/landing.js
@@ -10,14 +10,11 @@ class Landing extends Component {
 
   componentWillMount() {
     console.log("landing.js::componentWillMount invoked");
+    this.props.onReadGooglePhotos();
   }
 
   handleLoadGooglePhotos() {
     this.props.onLoadGooglePhotos();
-  }
-
-  handleReadGooglePhotos() {
-    this.props.onReadGooglePhotos();
   }
 
   handleReadDrivePhotos() {
@@ -32,7 +29,7 @@ class Landing extends Component {
 
     const style = {
       marginLeft: '2px',
-      marginTop: '16px',
+      marginTop: '2px',
       fontSize: '16px',
     };
 
@@ -40,21 +37,21 @@ class Landing extends Component {
 
       <MuiThemeProvider>
         <div>
-          <div>Pizza</div>
 
+          <h1>PhotoSyncATron</h1>
+          <h2>Match backup photos with Google photos</h2>
+          <h3>Load Google photos</h3>
+          <p>Click on Load from Cloud to load additional Google photos from the cloud (advanced)</p>
           <div>
             <RaisedButton
               onClick={this.handleLoadGooglePhotos.bind(this)}
-              label="Load Photos from Cloud"
-              style={style}
-            />
-            <RaisedButton
-              onClick={this.handleReadGooglePhotos.bind(this)}
-              label="Load Photos from File"
+              label="Load from Cloud"
               style={style}
             />
           </div>
 
+          <h3>Match photos</h3>
+          <p>Insert CD/DVD, enter the drive name and click on Match Photos to start the matching process.</p>
           <div>
             <div id="volumeName">
               <TextField
@@ -67,7 +64,7 @@ class Landing extends Component {
               />
               <RaisedButton
                 onClick={this.handleReadDrivePhotos.bind(this)}
-                label="Read Photos from Drive"
+                label="Match Photos"
                 style={style}
               />
 
