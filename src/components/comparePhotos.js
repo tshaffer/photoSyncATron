4882601545320fc5b807db94b1d7dc648a70437b
Ted Shaffer <shaffer.family@gmail.com>
2016-12-31 19:55:57 -0800

    show buttons on Compare Photos component.

--- src/components/comparePhotos.js
+++ src/components/comparePhotos.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
 
 class ComparePhotos extends Component {
 
@@ -14,28 +15,99 @@ class ComparePhotos extends Component {
   }
 
   componentWillMount() {
+
     console.log("comparePhotos.js::componentWillMount");
-    const diskImage = this.props.photoCompareList[0].baseFile;
-    const googleImage = this.props.photoCompareList[0].photoList[0].url;
+    console.log(this.props.photoCompareList);
+
+    this.numDrivePhotosToCompare = this.props.photoCompareList.length;
+    this.drivePhotoIndex = 0;
+
+    this.numGooglePhotosToCompare = this.props.photoCompareList[this.drivePhotoIndex].photoList.length;
+    this.googlePhotoIndex = 0;
+
+      // this.numGooglePhotosToCompare = this.props.photoCompareList.length;
+      // this.photoCompareListIndex = 0;
+      // this.photoCompareListGooglePhotoIndex = 0;
+      // this.photoCompareListNumGooglePhotos = this.props.photoCompareList[this.photoCompareListGooglePhotoIndex].photoList;
+
+    this.updatesPhotosToCompare();
+  }
+
+  updatesPhotosToCompare() {
+    const diskImage = this.props.photoCompareList[this.drivePhotoIndex].baseFile;
+    const googleImage = this.props.photoCompareList[this.drivePhotoIndex].photoList[this.googlePhotoIndex].url;
+
+    // const diskImage = this.props.photoCompareList[this.photoCompareListIndex].baseFile;
+    // const googleImage = this.props.photoCompareList[this.photoCompareListGooglePhotoIndex].photoList[0].url;
     this.setState({
-        diskImage,
-        googleImage
+      diskImage,
+      googleImage
     });
   }
 
+    // handlePhotosMatch() {
+    //
+    //
+    //     // this.photoCompareListIndex++;
+    //     // if (this.photoCompareListIndex >= this.numGooglePhotosToCompare) {
+    //     //     console.log("all comparisons complete - do something");
+    //     // }
+    //     // else {
+    //     //
+    //     // }
+    // }
+    //
+    // handlePhotosDontMatch() {
+    // }
+
+
   render () {
+
+    const style = {
+      marginLeft: '2px',
+      marginTop: '8px',
+      fontSize: '12px',
+    };
+
+      // {/*/!*<div>*!/*/}
+      //     {/*/!*<RaisedButton*!/*/}
+      //         {/*/!*onClick={this.handlePhotosMatch.bind(this)}*!/*/}
+      //         {/*/!*label="Photos Match"*!/*/}
+      //         {/*/!*style={style}*!/*/}
+      // //     {/*/>*/}
+      // //     {/*<RaisedButton*/}
+      // // //         onClick={this.handlePhotosDontMatch.bind(this)}
+      // // //         label="Photos Dont Match"
+      // // //         style={style}
+      // // //     />
+      // // //
+      // // // </div>
 
     return (
       <MuiThemeProvider>
-        <div className="allImages">
-          <img
-            className="leftImage"
-            src={this.state.diskImage}
-        />
-          <img
-            className="rightImage"
-            src={this.state.googleImage}
-        />
+        <div>
+          <div className="allImages">
+            <img
+              className="leftImage"
+              src={this.state.diskImage}
+                />
+            <img
+              className="rightImage"
+              src={this.state.googleImage}
+                />
+          </div>
+            <div className="clear">
+            </div>
+            <div>
+                <RaisedButton
+                    label="Photos Match"
+                    style={style}
+                />
+                <RaisedButton
+                    label="Photos Don't Match"
+                    style={style}
+                />
+            </div>
         </div>
       </MuiThemeProvider>
     );
