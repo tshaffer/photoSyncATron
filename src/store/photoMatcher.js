71b75ab7f5280611bb8b5a2fcbe54912e5d1966b
Ted Shaffer <shaffer.family@gmail.com>
2017-01-02 14:19:30 -0800

    another checkpoint - before deleting files.

--- src/store/photoMatcher.js
+++ src/store/photoMatcher.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs');
 const deepcopy = require("deepcopy");
 const sizeOf = require('image-size');
 
@@ -16,10 +17,82 @@ let photoDimensionsByName = {};
 // ------------------------------------
 // Constants
 // ------------------------------------
+const MATCH_ATTEMPT_COMPLETE = 'MATCH_ATTEMPT_COMPLETE';
+const PHOTO_MATCHING_COMPLETE = 'PHOTO_MATCHING_COMPLETE';
+const SET_PHOTO_COMPARE_LIST = 'SET_PHOTO_COMPARE_LIST';
+const SET_DRIVE_MATCH_RESULTS = 'SET_DRIVE_MATCH_RESULTS';
+const MATCH_FOUND = 'MATCH_FOUND';
+const NO_MATCH_FOUND = 'NO_MATCH_FOUND';
 
 // ------------------------------------
 // Helper functions
 // ------------------------------------
+function loadExistingSearchResults() {
+
+  return new Promise( (resolve) => {
+    fs.readFile('searchResults.json', (err, data) => {
+
+      let existingSearchResults;
+
+      if (err) {
+        existingSearchResults = {};
+        existingSearchResults.Volumes = {};
+      }
+      else {
+        existingSearchResults = JSON.parse(data);
+      }
+      resolve(existingSearchResults);
+    });
+  });
+}
+
+function buildManualPhotoMatchList(dispatch, searchResults) {
+
+  let photoCompareList = [];
+  searchResults.forEach( (searchResult) => {
+    if (searchResult.photoList) {
+      let photoCompareItem = {};
+      photoCompareItem.baseFile = searchResult.photoFile;
+      photoCompareItem.photoList = searchResult.photoList.photoList;
+      photoCompareList.push(photoCompareItem);
+    }
+  });
+
+  dispatch(setPhotoCompareList(photoCompareList));
+}
+
+function saveSearchResults(dispatch, searchResults) {
+
+  // build results based on this search
+  let matchPhotosResults = {};
+  let numMatchesFound = 0;
+  let numWithPhotoList = 0;
+
+  buildManualPhotoMatchList(dispatch, searchResults);
+
+  searchResults.forEach( (searchResult) => {
+
+    if (searchResult.success) {
+      numMatchesFound++;
+    }
+    else if (searchResult.photoList) {
+      numWithPhotoList++;
+      matchPhotosResults[searchResult.photoFile] = 'ManualMatchPending';
+    }
+    else {
+      matchPhotosResults[searchResult.photoFile] = searchResult.reason;
+    }
+  });
+
+  console.log('Total number of matches: ', numMatchesFound);
+  console.log('Number of potential matches: ', numWithPhotoList);
+
+  dispatch(setDriveMatchResults(matchPhotosResults));
+  dispatch(setPhotoMatchingComplete());
+  console.log("ALL DONE");
+}
+
+
 function findPhotoByName(getState, photoFile) {
 
   const photosByName = getState().googlePhotos.photosByName;
@@ -149,6 +222,8 @@ function setSearchResult(dispatch, photoFile, success, reason, error) {
 
 function matchPhotoFile(dispatch, getState, drivePhotoFile) {
 
+  let photosByExifDateTime = getState().googlePhotos.photosByExifDateTime;
+
   let searchResult = {};
 
   return new Promise( (resolve) => {
@@ -223,15 +298,56 @@ function matchPhotoFiles(dispatch, getState) {
 }
 
 // ------------------------------------
-// Action Creators
+// Actions
 // ------------------------------------
+function matchAttemptComplete(success) {
+  return {
+    type: MATCH_ATTEMPT_COMPLETE,
+    payload: success
+  };
+}
+
+function setPhotoCompareList(photoCompareList) {
+  return {
+    type: SET_PHOTO_COMPARE_LIST,
+    payload: photoCompareList
+  };
+}
+
+function setDriveMatchResults(driveResults) {
+  return {
+    type: SET_DRIVE_MATCH_RESULTS,
+    payload: driveResults
+  };
+}
+
+function setPhotoMatchingComplete() {
+  return {
+    type: PHOTO_MATCHING_COMPLETE
+  };
+}
+
+export function matchFound(photoFilePath) {
+  return {
+    type: MATCH_FOUND,
+    payload: photoFilePath
+  };
+}
+
+export function noMatchFound(photoFilePath) {
+  return {
+    type: NO_MATCH_FOUND,
+    payload: photoFilePath
+  };
+}
+
 
 // ------------------------------------
-// Actions
+// Actions Creators
 // ------------------------------------
 export function matchPhotos(volumeName) {
 
-  console.log("controller.js::matchPhotos");
+  console.log("photoMatcher.js::matchPhotos");
 
   return function (dispatch, getState) {
 
@@ -248,10 +364,67 @@ export function matchPhotos(volumeName) {
       matchPhotoFiles(dispatch, getState);
     });
     // TODO catch errors
-
-    // let state = getState();
-    // buildPhotoDictionaries(dispatch, state.googlePhotos);
-    // matchPhotoFiles(dispatch);
   };
 }
 
+export function saveResults() {
+
+  return function (_, getState) {
+
+    loadExistingSearchResults().then((searchResults) => {
+      // update data structure
+      searchResults.lastUpdated = new Date().toLocaleDateString();
+      const state = getState();
+      const volumeName = state.volumeName;
+      searchResults.Volumes[volumeName] = state.driveMatchResults;
+
+      // store search results in a file
+      const searchResultsStr = JSON.stringify(searchResults, null, 2);
+      fs.writeFileSync('searchResults.json', searchResultsStr);
+
+      console.log('searchResults.json saved');
+    });
+  };
+}
+
+// ------------------------------------
+// Reducer
+// ------------------------------------
+const initialState = {
+  successfulMatches: 0,
+  unsuccessfulMatches: 0,
+  photoMatchingComplete: false,
+  photoCompareList: []
+};
+
+export default function(state = initialState, action) {
+
+  switch (action.type) {
+
+    case MATCH_ATTEMPT_COMPLETE: {
+      let newState = Object.assign({}, state);
+      if (action.payload) {
+        newState.successfulMatches++;
+      }
+      else {
+        newState.unsuccessfulMatches++;
+      }
+
+      console.log('Successful matches, unsuccessful matches: ',
+        newState.successfulMatches, newState.unsuccessfulMatches);
+      return newState;
+    }
+    case PHOTO_MATCHING_COMPLETE: {
+      let newState = Object.assign({}, state);
+      newState.photoMatchingComplete = true;
+      return newState;
+    }
+    case SET_PHOTO_COMPARE_LIST: {
+      let newState = Object.assign({}, state);
+      newState.photoCompareList = action.payload;
+      return newState;
+    }
+  }
+
+  return state;
+}
