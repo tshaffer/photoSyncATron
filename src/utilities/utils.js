69f756202739b99780e13133abefd28ad2d4bd9b
Ted Shaffer <shaffer.family@gmail.com>
2017-01-21 07:48:18 -0800

    add guid() utility

--- src/utilities/utils.js
+++ src/utilities/utils.js
@@ -41,3 +41,14 @@ export function isNumeric(num: string){
   if (num === '') return false;
   return !isNaN(num);
 }
+
+export function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+}
+
